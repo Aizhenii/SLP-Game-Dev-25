972cbe88eab819489c14c77f62395a03bed5ab9e
BrettGit12 <brettlam2006@gmail.com>
2025-10-15 12:34:01 -0700

    I made a functional healthbar that responds to keyboard input. A decreases while D increases.

---
+++ Assets/PlaceholderScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderScript : MonoBehaviour
+{
+    public float Health, MaxHealth;
+    
+    [SerializeField]
+    private HealthBarUI healthBar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.SetMaxHealth(MaxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            SetHealth(-20f);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            SetHealth(20f);
+        }
+    }
+    
+    public void SetHealth(float healthChange)
+    {
+        Health += healthChange;
+        Health = Mathf.Clamp(Health, 0, MaxHealth);
+
+        healthBar.SetHealth(Health);
+    }
+}
