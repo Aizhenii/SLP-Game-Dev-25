f955c8dbe1ce7fb29ebee20430ccf47eb02091e3
MaliaWilken4 <mawilken4@gmail.com>
2025-11-10 22:54:48 -0800

    Imported new font asset into all popup screens, fixed paused menu logic

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,30 +10,39 @@ public class PauseMenu : MonoBehaviour
     public GameObject PauseOverlay;
     private bool isPaused = false;
 
+    public GameObject PausedRulesPanel;
+    private bool isRulesDisplayed = false;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
-                Resume();
-            else
+            if (!isPaused)
                 Pause();
+            else if (isRulesDisplayed)
+                CloseRules();
+            else
+                Resume();
         }
     }
 
     public void Pause()
     {
         PauseOverlay.SetActive(true);
+        PausedRulesPanel.SetActive(false);
         Time.timeScale = 0f;
         isPaused = true;
+        isRulesDisplayed = false;
     }
 
     public void Resume()
     {
         PauseOverlay.SetActive(false);
+        PausedRulesPanel.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        isRulesDisplayed = false;
     }
 
     public void RestartLevel()
@@ -42,10 +52,25 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene(currentScene.name);
     }
 
-    //
     public void QuitToMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Title_Scene");
+    }
+
+    public void DisplayRules()
+    {
+        Debug.Log("Rules Displayed");
+        PauseOverlay.SetActive(false);
+        PausedRulesPanel.SetActive(true);
+        isRulesDisplayed = true;
+    }
+
+    public void CloseRules()
+    {
+        Debug.Log("Rules Closed");
+        PausedRulesPanel.SetActive(false);
+        PauseOverlay.SetActive(true);
+        isRulesDisplayed = false;
     }
 }
