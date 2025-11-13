0c2cf4a6d5b02a2ce6229891bf8617cb31270d16
Elsa Zheng <zhengelsa150@gmail.com>
2025-11-13 10:18:44 -0800

    Bug fix for color placement
    - updated from overlay to world space camera

--- Assets/Scripts/TowerDragNDrop.cs
+++ Assets/Scripts/TowerDragNDrop.cs
@@ -2,98 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class TowerDragNDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler{
-    //[SerializeField] private Canvas canvas;
-    private RectTransform rectTransform;
-    private CanvasGroup canvasGroup;
+    private Camera mainCamera; //get camera
+    private CanvasGroup canvasGroup; 
     public static GameObject draggedTower; //tower being dragged
-    Vector3 startPosition;
-    Transform startParent;
-    /*
-    private Image assignedImage; //to hold sprite on the gameobject 
-    Color imageColor; //to store color for later revertion
-    */
+    private Vector3 startPosition; //initial position of tower
+    private Transform startParent; //original slot
+    private SpriteRenderer towerSprite; //to change the color later on
+    private Color originalColor; //original color of the game object
 
+    //starts when game begins
     private void Awake(){
-        rectTransform = GetComponent<RectTransform>();
+        mainCamera = Camera.main;
         canvasGroup = GetComponent<CanvasGroup>();
-        //assignedImage = GetComponent<Image>(); //access image
-        //imageColor = assignedImage.color; //get color
+        towerSprite = GetComponent<SpriteRenderer>(); //get the sprite
+        if (towerSprite != null){ 
+            originalColor = towerSprite.color; //get color of the sprite if it exists
+        }//end of if
     }//end of Awake
 
-    //what will happen when you start to click and start drag
     public void OnBeginDrag(PointerEventData eventData){
-        //Debug.Log("OnBeginDrag"); //check if it works
-        canvasGroup.alpha = .6f; //make tower transparent so you know item has been selected
-        //So the ray cast will ignore the item itself.
-        canvasGroup.blocksRaycasts = false; //prevent grid from being moved
-        startPosition = transform.position; //what is the parent (currently grid 1)
-        startParent = transform.parent;
-        transform.SetParent(transform.root); //no longer a child of the grid and just in the root 
-        draggedTower = gameObject;
+        if (canvasGroup != null){
+            canvasGroup.alpha = 0.6f; //semi transparent to indicate drag status
+            canvasGroup.blocksRaycasts = false; //ignore for taycasts to interact with it
+        }//end of if
+
+        startPosition = transform.position; //set new position when moved
+        startParent = transform.parent; //set new parent slot when moved 
+        draggedTower = gameObject; //reference to what is being moved
+        Debug.Log("Drag Begin");
     }//end of OnBeginDrag
 
-    //what happens during the drag
     public void OnDrag(PointerEventData eventData){
-        //So the tower will move with our mouse (at same speed)  and so it will be consistant if the canvas has a different scale (other then 1);
-        rectTransform.anchoredPosition += eventData.delta;///canvas.scaleFactor; //delta = speed of the mouse
+        //Convert mouse position to world position
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = Mathf.Abs(mainCamera.transform.position.z - transform.position.z);
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mouse);
 
-        /*
-        //see if position is valid using collider interaction
-        Vector3 mouse = Input.mousePosition; //get mouse position for checking
-        mouse.z = 0f; //2D not 3D
-        Vector3 world = Camera.main.ScreenToWorldPoint(mouse);
-        world.z = 0f;
-        Collider2D isHit = Physics2D.OverlapPoint(world); //for checking if mouse touches grid
-        bool isValidPosition = false; //checker initially false
-        if (isHit!=null && isHit.CompareTag("Grid")){ //if mouse hits grid, then change to true
-            isValidPosition = true;
-            Debug.Log("Allowed position");
-        }//end of else
-        checkValidPosition(isValidPosition); //send feedback to the player
-        */
+        //Move the tower to follow the mouse
+        transform.position = new Vector3(worldPos.x, worldPos.y, transform.position.z);
+
+        //Check if posiion is valid using tags and collider
+        Collider2D isHit = Physics2D.OverlapPoint(worldPos); //check if tower interacts with collider
+        bool isValidPosition = (isHit!=null && isHit.CompareTag("Grid")); //check if this is a grid to place on
+        checkValidPosition(isValidPosition);
+        Debug.Log("Dragging");
     }//end of OnDrag
 
-    //what happens at the end of the drag
     public void OnEndDrag(PointerEventData eventData){
-        draggedTower = null;
-
-        //moving the object into new grid
-        //will go back to last parent if tower is not close enough to another grid
-        if (transform.parent == startParent || transform.parent == transform.root){
-            transform.position = startPosition;
-            transform.SetParent(startParent);
+        draggedTower = null; //null when dragging ends because no need for reference now
+         
+        //reset to original position if no change
+        if (transform.parent == startParent){
+            transform.position = startPosition; 
         }//end of if
 
-        Debug.Log("OnEndDrag");
-        canvasGroup.alpha = 1f; //no longer transparent
-        canvasGroup.blocksRaycasts = true; //don't need this feature anymore
-        //resetColor(); //reset color when object is placed
+        //reset changes made during dragging
+        if (canvasGroup != null){
+            canvasGroup.alpha = 1f;
+            canvasGroup.blocksRaycasts = true;
+        }//end of if
+        resetColor(); //reset back to normal color
+        Debug.Log("End Drag");
     }//end of OnEndDrag
 
-    /*
-    //show the player if the position is valid, green = ok and red = bad
+    //tell player if position is valid or not
     public void checkValidPosition(bool isValid){
-        float alpha = .5f; //transparency level
-
-        if(assignedImage == null){
-            return; //do nothing if there is no tower image to alter
+        float alpha = 0.5f; //transparency level
+        if (towerSprite == null){
+            return;
         }//end of if
-
-        if (isValid){
-            assignedImage.color = new Color(0f, 1f, 0f, alpha); //green with 50% transparency
-        }//end of if
+        else if (isValid){
+            towerSprite.color = new Color(0f, 1f, 0f, alpha); //green for correct
+        }//end of else if
         else{
-            assignedImage.color = new Color(1f, 0f, 0f, alpha); //red with 50% transparency
+            towerSprite.color = new Color(1f, 0f, 0f, alpha); //red for incorrect
         }//end of else
     }//end of checkValidPosition
 
+    //set tower back to original sprite and color
     public void resetColor(){
-        if(assignedImage != null){
-            assignedImage.color = new Color(imageColor.r, imageColor.g, imageColor.b, 1f);
+        if (towerSprite != null){
+            towerSprite.color = originalColor;
         }//end of if
     }//end of resetColor
-    */
-}//end of TowerDragNDrop class
+}//end of TowerDragNDrop
+
