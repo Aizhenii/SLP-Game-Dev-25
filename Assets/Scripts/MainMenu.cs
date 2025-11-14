19d313e2f665c06c835a1fddd6b96e27bfb7cc67
Elsa Zheng <zhengelsa150@gmail.com>
2025-11-14 01:15:59 -0800

    Button Updates
    Use built in Unity function, previous script had a weird bug where it played on every click

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,14 +10,6 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenuPanel;
     public bool isDisplayed = false;
 
-    [SerializeField] private AudioClip hitMainBTN; //sound for pushing the button
-    private AudioSource audioSource; //to play sound effects
-
-    //Start is called before the first frame update
-    void Start(){
-        audioSource = GetComponent<AudioSource>(); //initialize   
-    }//end of Start
-
     public void PlayGame()
     {
         SceneManager.LoadScene("Level_1_Scene");
@@ -40,14 +32,6 @@ public class MainMenu : MonoBehaviour
 
     public void Update()
     {
-        //detect user click, can modify if you want -Elsa
-        //only does sound features
-        if (Input.GetMouseButton(0)){ //left click
-            //play sound when button is pressed
-            if (hitMainBTN != null)
-                audioSource.PlayOneShot(hitMainBTN, 0.25f);
-        }//end of if
-
         // Rules Panel is Displayed and User Presses Escape
         if (isDisplayed && Input.GetKeyDown(KeyCode.Escape))
         {
